834b832684e0fcf38daa4071ffd502419da0c201
Andy Holland <andyholland1991@aol.com>
2016-04-29 02:29:21 +0100

    Setup rubocop on CC

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,3 +5,8 @@ require 'dry/configurable'
 Dir[Pathname(__FILE__).dirname.join('support/**/*.rb').to_s].each do |file|
   require file
 end
+
+if RUBY_ENGINE == "rbx"
+  require "codeclimate-test-reporter"
+  CodeClimate::TestReporter.start
+end
