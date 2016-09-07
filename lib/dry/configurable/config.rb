2ac40820e99abed4bbe6b3a7c95be4e015b6d11e
Andy Holland <andyholland1991@aol.com>
2016-09-07 23:15:12 +0100

    Support value processor using a block argument with parameters passed to #setting

--- lib/dry/configurable/config.rb
+++ lib/dry/configurable/config.rb
@@ -1,10 +1,61 @@
 module Dry
   module Configurable
     # @private
-    class Config < ::Struct
-      def self.create(hash)
-        self.new(*hash.keys).new(*hash.values)
+    class Config
+      DEFAULT_PROCESSOR = ->(v) { v }.freeze
+
+      def self.create(settings)
+        klass = ::Class.new(self)
+
+        settings.each do |setting|
+          klass.__send__(:define_method, setting.name) do
+            @config[setting.name]
+          end
+
+          klass.__send__(:define_method, "#{setting.name}=") do |value|
+            @config[setting.name] = setting.processor.call(value)
+          end
+        end
+
+        klass.new(settings)
       end
+
+      def initialize(settings)
+        @config = ::Concurrent::Hash.new
+
+        settings.each do |setting|
+          if setting.none?
+            @config[setting.name] = nil
+          else
+            public_send("#{setting.name}=", setting.value)
+          end
+        end
+      end
+
+      def dup
+        dup = super
+        dup.instance_variable_set(:@config, @config.dup)
+        dup
+      end
+
+      def clone
+        clone = super
+        clone.instance_variable_set(:@config, @config.clone)
+        clone
+      end
+
+      def to_h
+        @config.each_with_object({}) do |tuple, hash|
+          key, value = tuple
+
+          if value.kind_of?(::Dry::Configurable::Config)
+            hash[key] = value.to_h
+          else
+            hash[key] = value 
+          end
+        end
+      end
+      alias to_hash to_h
     end
   end
 end
