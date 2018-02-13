5054178340aff0160079d95e0a88d4e482256eee
GustavoCaso <gustavocaso@gmail.com>
2018-02-13 13:15:17 +0100

    Delete old files and test files

--- lib/dry/configurable/error.rb
+++
@@ -1,9 +0,0 @@
-# A collection of micro-libraries, each intended to encapsulate
-# a common task in Ruby
-module Dry
-  module Configurable
-    Error = Class.new(::StandardError)
-    AlreadyDefinedConfig = ::Class.new(Error)
-    FrozenConfig = ::Class.new(Error)
-  end
-end
