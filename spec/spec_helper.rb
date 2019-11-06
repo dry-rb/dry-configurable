b39639358fe692969db56dce9dd095cd159cb748
Nikita Shilnikov <fg@flashgordon.ru>
2019-11-06 18:34:48 +0300

    Setup GH Actions

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,13 @@
-if RUBY_ENGINE == 'ruby' && ENV['COVERAGE'] == 'true'
-  require 'yaml'
-  rubies = YAML.load(File.read(File.join(__dir__, '..', '.travis.yml')))['rvm']
-  latest_mri = rubies.select { |v| v =~ /\A\d+\.\d+.\d+\z/ }.max
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 
-  if RUBY_VERSION == latest_mri
-    require 'simplecov'
-    SimpleCov.start do
-      add_filter '/spec/'
-    end
+require 'pathname'
+
+SPEC_ROOT = Pathname(__FILE__).dirname
+
+if ENV['COVERAGE'] == 'true'
+  require 'simplecov'
+  SimpleCov.start do
+    add_filter '/spec/'
   end
 end
 
