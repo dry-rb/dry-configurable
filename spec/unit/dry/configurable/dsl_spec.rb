41559e603e6ebd942d86728af166fa80131b383a
Nikita Shilnikov <fg@flashgordon.ru>
2022-07-27 14:17:56 +0300

    Refactor deprecations specs

--- spec/unit/dry/configurable/dsl_spec.rb
+++ spec/unit/dry/configurable/dsl_spec.rb
@@ -7,6 +7,21 @@ RSpec.describe Dry::Configurable::DSL do
     Dry::Configurable::DSL.new
   end
 
+  let(:logger) { StringIO.new }
+
+  let(:logged) do
+    logger.rewind
+    logger.string
+  end
+
+  around(:each, :collect_deprecations) do |example|
+    Dry::Core::Deprecations.set_logger!(logger)
+
+    example.run
+  ensure
+    Dry::Core::Deprecations.set_logger!(STDOUT)
+  end
+
   it "compiles a setting with no options" do
     setting = dsl.setting :user
 
@@ -21,69 +36,54 @@ RSpec.describe Dry::Configurable::DSL do
     expect(setting.value).to eql("root")
   end
 
-  it "compiles but deprecates giving a default as positional argument" do
-    logger = StringIO.new
-    Dry::Core::Deprecations.set_logger!(logger)
+  it "compiles but deprecates giving a default as positional argument", :collect_deprecations do
     setting = dsl.setting :user, "root"
 
     expect(setting.name).to be(:user)
     expect(setting.value).to eql("root")
-    logger.rewind
-    expect(logger.string).to match(/default value as positional argument to settings is deprecated/)
+    expect(logged).to match(/default value as positional argument to settings is deprecated/)
   end
 
-  it "compiles when giving a default as positional argument, and suppresses the warning when flagged off" do
+  it "compiles when giving a default as positional argument, and suppresses the warning when flagged off", :collect_deprecations do
     Dry::Configurable.warn_on_setting_positional_default false
 
-    logger = StringIO.new
-    Dry::Core::Deprecations.set_logger!(logger)
     setting = dsl.setting :user, "root"
 
     expect(setting.name).to be(:user)
     expect(setting.value).to eql("root")
-    logger.rewind
-    expect(logger.string).to be_empty
+    expect(logged).to be_empty
 
     Dry::Configurable.warn_on_setting_positional_default true
   end
 
-  it "compiles but deprecates giving a defalt hash value as a positional argument (without any keyword args)" do
+  it "compiles but deprecates giving a defalt hash value as a positional argument (without any keyword args)", :collect_deprecations do
     # This test is necessary for behavior specific to Ruby 2.6 and 2.7
-
-    logger = StringIO.new
-    Dry::Core::Deprecations.set_logger!(logger)
 
     setting = dsl.setting :default_options, {foo: "bar"}
 
     expect(setting.name).to be(:default_options)
     expect(setting.value).to eq(foo: "bar")
-    logger.rewind
-    expect(logger.string).to match(/default value as positional argument to settings is deprecated/)
+    expect(logged).to match(/default value as positional argument to settings is deprecated/)
+  end
 
-    if RUBY_VERSION < "3.0"
-      logger = StringIO.new
-      Dry::Core::Deprecations.set_logger!(logger)
-
+  if RUBY_VERSION < "3.0"
+    it "compiles but deprecates giving a default hash value as a positional argument (with keyword args)", :collect_deprecations do
       setting = dsl.setting :default_options, foo: "bar"
 
       expect(setting.name).to be(:default_options)
       expect(setting.value).to eq(foo: "bar")
-      logger.rewind
-      expect(logger.string).to match(/default value as positional argument to settings is deprecated/)
+      expect(logged).to match(/default value as positional argument to settings is deprecated/)
     end
   end
 
-  it "compiles but deprecates giving a defalt hash value as a positional argument (with keyword args)" do
+  it "compiles but deprecates giving a defalt hash value as a positional argument (with keyword args)", :collect_deprecations do
     # This test is necessary for behavior specific to Ruby 2.6 and 2.7
 
-    logger = StringIO.new
-    Dry::Core::Deprecations.set_logger!(logger)
     setting = dsl.setting :default_options, {foo: "bar"}, reader: true
 
     expect(setting.name).to be(:default_options)
     expect(setting.value).to eq(foo: "bar")
-    logger.rewind
-    expect(logger.string).to match(/default value as positional argument to settings is deprecated/)
+    expect(logged).to match(/default value as positional argument to settings is deprecated/)
   end
 
   it "does not infer a default hash value when non-valid keyword arguments are mixed in with valid keyword arguments" do
@@ -122,30 +122,22 @@ RSpec.describe Dry::Configurable::DSL do
     expect(setting.value).to eql("jdbc:sqlite")
   end
 
-  it "supports but deprecates giving a constructor as a block" do
-    logger = StringIO.new
-    Dry::Core::Deprecations.set_logger!(logger)
-
+  it "supports but deprecates giving a constructor as a block", :collect_deprecations do
     setting = dsl.setting(:dsn, default: "sqlite") { |value| "jdbc:#{value}" }
 
     expect(setting.name).to be(:dsn)
     expect(setting.value).to eql("jdbc:sqlite")
-    logger.rewind
-    expect(logger.string).to match(/constructor as a block is deprecated/)
+    expect(logged).to match(/constructor as a block is deprecated/)
   end
 
-  it "supports but deprecates giving a constructor as a block, and suppresses the warning when flagged off" do
+  it "supports but deprecates giving a constructor as a block, and suppresses the warning when flagged off", :collect_deprecations do
     Dry::Configurable.warn_on_setting_constructor_block false
-
-    logger = StringIO.new
-    Dry::Core::Deprecations.set_logger!(logger)
 
     setting = dsl.setting(:dsn, default: "sqlite") { |value| "jdbc:#{value}" }
 
     expect(setting.name).to be(:dsn)
     expect(setting.value).to eql("jdbc:sqlite")
-    logger.rewind
-    expect(logger.string).to be_empty
+    expect(logged).to be_empty
 
     Dry::Configurable.warn_on_setting_constructor_block true
   end
