a7cec82081cbea823f1eae177074cd68e98144bc
Tim Riley <tim@riley.id.au>
2022-08-24 22:03:22 +1000

    Add memory usage benchmark (#137)
    This compares the memory cost of inheritance from the following superclasses:
    
    - dry-configurable using two top-level settings
    - dry-configurable using the same two settings, just nested one level deeper
    - dry-core class attributes
    - hanami-utils class attributes
    - a plain old Ruby class, serving as a "control" for the benchmark

---
+++ benchmarks/memory.rb
@@ -0,0 +1,89 @@
+# frozen_string_literal: true
+
+require "benchmark/memory"
+
+def inherit_configurable(times)
+  require "dry/configurable"
+
+  klass = Class.new do
+    extend Dry::Configurable
+
+    setting :user, default: "jane"
+    setting :pass, default: "abcd"
+  end
+
+  times.times do
+    _subclass = Class.new(klass)
+  end
+end
+
+def inherit_configurable_nested(times)
+  require "dry/configurable"
+
+  klass = Class.new do
+    extend Dry::Configurable
+
+    setting :db do
+      setting :user, default: "jane"
+      setting :pass, default: "abcd"
+    end
+  end
+
+  times.times do
+    _subclass = Class.new(klass)
+  end
+end
+
+def inherit_dry_class_attributes(times)
+  require "dry/core/class_attributes"
+
+  klass = Class.new do
+    extend Dry::Core::ClassAttributes
+
+    defines :user, :pass
+
+    user "jane"
+    pass "abcd"
+  end
+
+  times.times do
+    _subclass = Class.new(klass)
+  end
+end
+
+def inherit_hanami_class_attributes(times)
+  require "hanami/utils/class_attribute"
+
+  klass = Class.new do
+    include Hanami::Utils::ClassAttribute
+
+    class_attribute :user, :pass
+
+    self.user = "jane"
+    self.pass = "abcd"
+  end
+
+  times.times do
+    _subclass = Class.new(klass)
+  end
+end
+
+def inherit_ordinary_class(times)
+  klass = Class.new
+
+  times.times do
+    _subclass = Class.new(klass)
+  end
+end
+
+times = Integer(ENV.fetch("TIMES", 100))
+
+Benchmark.memory do |x|
+  x.report("configurable") { inherit_configurable(times) }
+  x.report("configurable (nested)") { inherit_configurable_nested(times) }
+  x.report("dry-core class attributes") { inherit_dry_class_attributes(times) }
+  x.report("hanami-utils class attributes") { inherit_hanami_class_attributes(times) }
+  x.report("ordinary inheritance") { inherit_ordinary_class(times) }
+
+  x.compare!
+end
