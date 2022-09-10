4468aa584dd0ac2a3eaaa6c1e7976a08c03c8843
Tim Riley <tim@riley.id.au>
2022-09-11 00:01:41 +1000

    We save a fair amount of memory by not using Concurrent::Map

--- lib/dry/configurable/settings.rb
+++ lib/dry/configurable/settings.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require "concurrent/map"
-
 module Dry
   module Configurable
     # A settings map
@@ -17,7 +15,7 @@ module Dry
 
       # @api private
       def initialize(settings = EMPTY_ARRAY)
-        @settings = settings.each_with_object(Concurrent::Map.new) { |s, m| m[s.name] = s }
+        @settings = settings.each_with_object({}) { |s, m| m[s.name] = s }
       end
 
       # @api private
