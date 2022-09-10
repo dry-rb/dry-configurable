10d4af775166cdeb81161746b3e4c333560bbb89
Tim Riley <tim@riley.id.au>
2022-09-11 00:54:31 +1000

    Use original settings object for included config
    Now that settings and config are separated, we don't need to dup the settings anymore. This allows the config to use settings defined even after instances of the configurable class are made.

--- lib/dry/configurable/instance_methods.rb
+++ lib/dry/configurable/instance_methods.rb
@@ -12,10 +12,7 @@ module Dry
     module Initializer
       # @api private
       def initialize(*)
-        # Dup settings at time of initializing to ensure setting values are specific to
-        # this instance. This does mean that any settings defined on the class _after_
-        # initialization will not be available on the instance.
-        @config = self.class.__config_build__(self.class._settings.dup)
+        @config = self.class.__config_build__(self.class._settings)
 
         super
       end
