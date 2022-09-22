a18665edb96129a706e759cd4863b544f91d0e50
Tim Riley <tim@riley.id.au>
2022-09-22 19:59:50 +1000

    Remove unneeded requires

--- lib/dry/configurable/instance_methods.rb
+++ lib/dry/configurable/instance_methods.rb
@@ -1,8 +1,5 @@
 # frozen_string_literal: true
 
-require "dry/configurable/config"
-require "dry/configurable/methods"
-
 module Dry
   module Configurable
     # Initializer method which is prepended when `Dry::Configurable`
