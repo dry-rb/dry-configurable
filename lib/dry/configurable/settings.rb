9c25345a7d46f10477ea3fb78a190446f7b33fa9
Tim Riley <tim@riley.id.au>
2022-10-12 22:18:45 +1100

    Cache all_settings

--- lib/dry/configurable/settings.rb
+++ lib/dry/configurable/settings.rb
@@ -32,17 +32,23 @@ module Dry
       private def initialize_copy(source)
         @parent_settings = source.parent_settings.dup
         @settings = source.settings.dup
+        @lookup_cache = source.lookup_cache.dup
       end
 
       # @api private
       def dup_for_child
-        dup.tap do |child_settings|
-          child_settings.parent_settings << settings
-        end
+        dup.tap { |child| child.add_parent(settings) }
+      end
+
+      # @api private
+      def add_parent(parent)
+        # Use length for all_settings cache invalidation
+        parent_settings << [parent, parent.length]
       end
 
       # @api private
       def <<(setting)
+        @all_settings = nil # bit gross
         settings[setting.name] = setting
         self
       end
@@ -56,14 +62,27 @@ module Dry
 
       # @api private
       def each(&block)
-        parent_settings.each { |parent| parent.each_value(&block) }
-        settings.each_value(&block)
+        all_settings.each_value(&block)
       end
 
       private
 
       def all_settings
-        {**parent_settings.reduce({}, :merge), **settings}
+        if parent_settings.none?
+          settings
+        elsif @all_settings && parent_settings.all? { |(parent, cached_length)| parent.length == cached_length }
+          # Our cache is valid; no new parent settings have been defined since it was built
+          @all_settings
+        else
+          @all_settings = {}.tap { |hsh|
+            parent_settings.each_with_index do |(parent, _length), index|
+              parent_settings[index][1] = parent.length
+              hsh.merge!(parent)
+            end
+
+            hsh.merge!(settings)
+          }
+        end
       end
     end
   end
